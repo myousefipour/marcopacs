d39bf51a45a4240d0c43819aeb0b297d2392df4a
root <root@localhost.localdomain>
2018-09-30 22:36:17 +0330

    completed call soap service

--- client/components/soap/soap.js
+++ client/components/soap/soap.js
@@ -11,14 +11,25 @@ Template.soap.helpers({
 }); 
 
 Template.soap.events({ 
-    'click button.btn-load-soap': function(event, template) { 
-        Meteor.call('loadSoap', function (error, result) {
+    'click button.btn-load-describe': function(event, template) { 
+        Meteor.call('loadDescribe',$('.txt-soap-url').val().toString(), function (error, result) {
 			if (error) {
 				console.log('error', error);
 			}
 			else {
 				console.log('load soap successed...');
-				console.log(result);
+				$($('div.soap-describe-body')[0]).text(JSON.stringify(result));
+			}
+		});
+    } ,
+    'click button.btn-load-soap': function(event, template) { 
+        Meteor.call('loadSoapMethod',$('.txt-soap-url').val().toString(), function (error, result) {
+			if (error) {
+				console.log('error', error);
+			}
+			else {
+				console.log('load soap successed...');
+				$($('div.soap-method-body')[0]).text(JSON.stringify(result));
 			}
 		});
     } 
