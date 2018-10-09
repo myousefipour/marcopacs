1bc96efa657a5c5e35257003fd4f4e401f66cb58
root <root@localhost.localdomain>
2018-10-09 16:45:28 +0330

    create chat panel ui

---
+++ client/chat/chat.js
@@ -0,0 +1,14 @@
+Template.chat.events({
+    'click button.btn-submit-auth': function (event, template) {
+        // Meteor.call('chatAthenticate', $('#exampleInputEmail1').val(), $('#exampleInputPassword1').val(),$('#exampleInputPerson').val(), function (error, result) {
+        //     if (error) {
+        //         console.log(error);
+        //     }
+        //     else {
+        //         console.log(result);
+        //     }
+        // });
+
+
+    }
+});
