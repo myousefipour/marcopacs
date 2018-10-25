a7258dec03d6cf10da2d5db3bcbe9a78cdd302ca
root <root@localhost.localdomain>
2018-10-25 20:23:37 +0330

    rocket chat panel

--- client/chat/chat.js
+++ client/chat/chat.js
@@ -1,14 +1,38 @@
-Template.chat.events({
-    'click button.btn-submit-auth': function (event, template) {
-        // Meteor.call('chatAthenticate', $('#exampleInputEmail1').val(), $('#exampleInputPassword1').val(),$('#exampleInputPerson').val(), function (error, result) {
-        //     if (error) {
-        //         console.log(error);
-        //     }
-        //     else {
-        //         console.log(result);
-        //     }
-        // });
+import { HTTP } from 'meteor/http';
+var unirest = require('unirest');
+var RocketChat = require('rocketchat-nodejs').Client;
+if (Meteor.isClient) {
+    var me;
+    var Client = new RocketChat({
+        host: 'open.rocket.chat/home',
+        scheme: 'https', 
+        username: 'mukhtar',
+        password: 'r09371420709'
+    });
+    Client.login().then(() => {
+        var Authentication = Client.Authentication();
+        var Miscellaneous = Client.Miscellaneous();
+        // /api/v1/info
+        Miscellaneous.info().then((result) => {
+            var info = result;
+        }).catch((error) => {
+            console.log(error)
+        });
+
+        console.log('login success');
+        // console.log(result);
+        // write your API functions here
+        // examples
+        Authentication.me().then((result) => {
+            me = result;
+            $('#meesage-panel').html(result);
+        });
+
+    }).catch((error) => {
+        console.log(error);
+    });
+
+}
 
 
-    }
-});
+
